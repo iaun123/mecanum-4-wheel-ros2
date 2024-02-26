f2246fc1e5cb4ae552060a976698fc9def94cca2
iaun123 <145544320+iaun123@users.noreply.github.com>
2024-02-26 15:13:37 +0700

    Update robot.launch.py

--- iron_x/launch/robot.launch.py
+++ iron_x/launch/robot.launch.py
@@ -16,21 +16,17 @@ def generate_launch_description():
     model_file = os.path.join(iron_x_dir, "urdf", "gazebo.urdf")
     rviz_file = os.path.join(iron_x_dir, "rviz", "basic.rviz")
     world_file = os.path.join(iron_x_dir, "worlds", "stage.world")
-
     # Pose where we want to spawn the robot
     spawn_x_val = '-2.2'
     spawn_y_val = '0.08'
     spawn_z_val = '0.0'
     spawn_yaw_val = '0.0'
-
     declare_use_rviz = DeclareLaunchArgument(name='use_rviz', default_value='True')
     declare_use_sim_time = DeclareLaunchArgument(name='use_sim_time', default_value='false')
-    
     use_rviz =  LaunchConfiguration('use_rviz')
     use_sim_time = LaunchConfiguration('use_sim_time')
     robot_description = xacro.process_file(model_file)
     env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", iron_x_share)
-   
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -38,18 +34,15 @@ def generate_launch_description():
                      "robot_description": robot_description.toxml()}],
         arguments=[model_file]
     )
-
     gazebo_server = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros_dir, "launch", "gzserver.launch.py"),),
         launch_arguments={"world": world_file}.items()
     )
-
     gazebo_client = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros_dir, "launch", "gzclient.launch.py"))
     )
-
     spawn_robot = Node(package="gazebo_ros", executable="spawn_entity.py",
                        arguments=["-entity", "iron_x",
                                   "-topic", "robot_description",
@@ -60,7 +53,6 @@ def generate_launch_description():
                                 ],
                        output="screen",
     )   
-    
     rviz_node = Node(
         condition=IfCondition(use_rviz),
         package="rviz2",
@@ -69,7 +61,6 @@ def generate_launch_description():
         arguments=["-d", rviz_file],
         parameters=[{'use_sim_time': use_sim_time}]
     )
-
     robot_localization_node = launch_ros.actions.Node(
        package='robot_localization',
        executable='ekf_node',
@@ -78,11 +69,9 @@ def generate_launch_description():
        parameters=[os.path.join(iron_x_share, 'config/ekf.yaml'), 
                    {'use_sim_time': use_sim_time}]
     )
-
     return LaunchDescription([
         declare_use_rviz,
         declare_use_sim_time,
-
         env_var,
         gazebo_server,
         gazebo_client,
