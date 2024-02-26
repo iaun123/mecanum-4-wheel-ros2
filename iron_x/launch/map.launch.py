6d10c67b1cea4bfad3b7605c13b769c8139d0a35
iaun123 <145544320+iaun123@users.noreply.github.com>
2024-02-26 12:51:01 +0700

    Add files via upload

---
+++ iron_x/launch/map.launch.py
@@ -0,0 +1,66 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+# TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    map_dir = LaunchConfiguration('map',default=os.path.join(
+        get_package_share_directory('iron_x'),'maps','cafe','cafe.yaml'))
+
+    param_file_name = 'test.yaml'
+    param_dir = LaunchConfiguration(
+        'params',default=os.path.join(get_package_share_directory('iron_x'),
+            'params',param_file_name))
+
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),'rviz','nav2_default_view.rviz')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params',
+            default_value=param_dir,
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params': param_dir}.items(),),
+
+        Node(
+            package='rviz2',
+            node_executable='rviz2',
+            node_name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='tf2',
+        #     arguments = ["0", "0", "0", "0", "0", "0", "odom", "map"] 
+        #     # Translation.x, Translation.y, Translation.z, Rotation.yaw, Rotation.pitch, Rotation.roll, Parent frame, child frame
+        # )
+    ])
